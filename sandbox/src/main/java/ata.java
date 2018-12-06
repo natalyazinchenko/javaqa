63cd5d32dd49a817c0ee7fa83043c19ac3ef3cdd
natalyazinchenko <natalyazinchenko@nixsolutions.com>
2018-12-06 12:44:50 +0200

    functions added

--- sandbox/src/main/java/ata.java
+++ sandbox/src/main/java/ata.java
@@ -1,7 +1,27 @@
 public class ata {
 
 public static void main(String [] args) {
+    hello("world");
+    hello("user");
+    hello("Natasha");
 
-System.out.println("Hello, world!");
+    double len = 5;
+    System.out.println("Square " + len + " equals: " + area(len));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Square pryamougolnika so storonami " + a + " i " + b + " ravno " + areasq(a,b));
+}
+
+public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+}
+
+public static double area(double l) {
+    return l * l;
+}
+
+public static double areasq(double a, double b) {
+    return a * b;
 }
 }
